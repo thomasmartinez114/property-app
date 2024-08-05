7dec72dce32f9a1470292a62d1c6e5716cacbf53
Thomas Martinez <devtommie114@gmail.com>
2024-08-04 23:36:52 -0400

    folder/file based routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-    return ( <div className="text-2xl">HomePage</div> );
-}
- 
+  return (
+    <div>
+      <h1 className='text-3xl'>Welcome</h1>
+      <Link href='/properties'>Go to Properties</Link>
+    </div>
+  );
+};
+
 export default HomePage;
