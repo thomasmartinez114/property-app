464130abf1471bc17feb3e4f95ce5341fd328cb1
Thomas Martinez <devtommie114@gmail.com>
2024-08-07 18:48:00 -0400

    infoboxes displaying with props

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,13 +1,19 @@
-const InfoBox = ({ heading, children }) => {
+const InfoBox = ({
+  heading,
+  backgroundColor = 'bg-gray-100',
+  textColor = 'text-gray-800',
+  description,
+  buttonInfo,
+}) => {
   return (
-    <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
-      <h2 className='text-2xl font-bold'>{heading}</h2>
-      <p className='mt-2 mb-4'>{children}</p>
+    <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{description}</p>
       <a
-        href='/properties.html'
-        className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'
+        href={buttonInfo.link}
+        className={`${buttonInfo.backgroundColor} inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
       >
-        Browse Properties
+        {buttonInfo.text}
       </a>
     </div>
   );
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -5,14 +5,27 @@ const InfoBoxes = () => {
     <section>
       <div className='container-xl lg:container m-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
-          <InfoBox heading='For Renters'>
-            Find your dream rental property. Bookmark properties and contact
-            owners.
-          </InfoBox>
-          <InfoBox heading='For Property Owners'>
-            List your properties and reach potential tenants. Rent as an airbnb
-            or long term.
-          </InfoBox>
+          <InfoBox
+            heading='For Renters'
+            description='Find your dream rental property. Bookmark properties and contact
+            owners.'
+            buttonInfo={{
+              text: 'Browse Properties',
+              link: '/properties',
+              backgroundColor: 'bg-black',
+            }}
+          />
+          <InfoBox
+            heading='For Property Owners'
+            backgroundColor='bg-blue-100'
+            description='List your properties and reach potential tenants. Rent as an airbnb
+            or long term.'
+            buttonInfo={{
+              text: 'Add Property',
+              link: '/properties/add',
+              backgroundColor: 'bg-blue-500',
+            }}
+          />
         </div>
       </div>
     </section>
