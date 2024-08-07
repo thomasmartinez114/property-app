21f3f61c0f974fe2f51601837a8f487dfc40a69e
Thomas Martinez <devtommie114@gmail.com>
2024-08-06 23:48:14 -0400

    props passing to InfoBoxes for Heading

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,5 +1,16 @@
-const InfoBox = () => {
-  return <div>InfoBox</div>;
+const InfoBox = ({ heading, children }) => {
+  return (
+    <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
+      <h2 className='text-2xl font-bold'>{heading}</h2>
+      <p className='mt-2 mb-4'>{children}</p>
+      <a
+        href='/properties.html'
+        className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'
+      >
+        Browse Properties
+      </a>
+    </div>
+  );
 };
 
 export default InfoBox;
