e7698e465853d7cebe28d7b13038d8d4338d04f0
Thomas Martinez <devtommie114@gmail.com>
2024-08-06 23:06:54 -0400

    add property link only show when logged in

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -76,14 +76,16 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                <Link
-                  href='/properties/add'
-                  className={`${
-                    pathname === '/properties/add' ? 'bg-black' : ''
-                  } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
-                >
-                  Add Property
-                </Link>
+                {isLoggedIn && (
+                  <Link
+                    href='/properties/add'
+                    className={`${
+                      pathname === '/properties/add' ? 'bg-black' : ''
+                    } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                  >
+                    Add Property
+                  </Link>
+                )}
               </div>
             </div>
           </div>
@@ -215,14 +217,16 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            <Link
-              href='/properties/add'
-              className={`${
-                pathname === '/properties/add' ? 'bg-black' : ''
-              } text-white block rounded-md px-3 py-2 text-base font-medium`}
-            >
-              Add Property
-            </Link>
+            {isLoggedIn && (
+              <Link
+                href='/properties/add'
+                className={`${
+                  pathname === '/properties/add' ? 'bg-black' : ''
+                } text-white block rounded-md px-3 py-2 text-base font-medium`}
+              >
+                Add Property
+              </Link>
+            )}
             {!isLoggedIn && (
               <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5'>
                 <i className='fa-brands fa-google mr-2'></i>
