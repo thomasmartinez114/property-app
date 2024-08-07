3df4eda67f134921f8dbe8e2e1360b6d89ef3d95
Thomas Martinez <devtommie114@gmail.com>
2024-08-06 22:11:35 -0400

    added Google icon / installed react-icons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import logo from '@/assets/images/logo-white.png';
 import Image from 'next/image';
 import profileDefault from '@/assets/images/profile.png';
 import Link from 'next/link';
+import { FaGoogle } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -74,7 +75,7 @@ const Navbar = () => {
           <div className='hidden md:block md:ml-6'>
             <div className='flex items-center'>
               <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                <i className='fa-brands fa-google text-white mr-2'></i>
+                <FaGoogle className='text-white mr-2' />
                 <span>Login or Register</span>
               </button>
             </div>
