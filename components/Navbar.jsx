21d6131dfbef8b84df3b99019a381f33f2528e13
Thomas Martinez <devtommie114@gmail.com>
2024-08-06 23:05:33 -0400

    mobile menu: if not logged in show the login button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -223,10 +223,12 @@ const Navbar = () => {
             >
               Add Property
             </Link>
-            <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5'>
-              <i className='fa-brands fa-google mr-2'></i>
-              <span>Login or Register</span>
-            </button>
+            {!isLoggedIn && (
+              <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5'>
+                <i className='fa-brands fa-google mr-2'></i>
+                <span>Login or Register</span>
+              </button>
+            )}
           </div>
         </div>
       )}
