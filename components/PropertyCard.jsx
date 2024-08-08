34e94ebd86fb442846963c7981bc2eefd40e93db
Thomas Martinez <devtommie114@gmail.com>
2024-08-07 20:06:42 -0400

    property link details

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -64,12 +64,12 @@ const PropertyCard = ({ property }) => {
               {', '} {property.location.state}
             </span>
           </div>
-          <a
-            href='property.html'
+          <Link
+            href={`/properties/${property._id}`}
             className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'
           >
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
