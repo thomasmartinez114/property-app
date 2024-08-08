9a1df80dc5fb5d9f687c647e28abf3173df8e94e
Thomas Martinez <devtommie114@gmail.com>
2024-08-07 20:36:05 -0400

    getRateDisplay

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -9,6 +9,18 @@ import {
 } from 'react-icons/fa';
 
 const PropertyCard = ({ property }) => {
+  // Get rate if monthly/weekly/nightly
+  const getRateDisplay = () => {
+    const { rates } = property;
+    if (rates.monthly) {
+      return `$${rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      return `$${rates.weekly.toLocaleString()}/wk`;
+    } else if (rates.nightly) {
+      return `$${rates.nightly.toLocaleString()}/night`;
+    }
+  };
+
   return (
     <div className='rounded-xl shadow-md relative'>
       <Image
@@ -25,7 +37,7 @@ const PropertyCard = ({ property }) => {
           <h3 className='text-xl font-bold'>{property.name}</h3>
         </div>
         <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
-          $4,200/mo
+          {getRateDisplay()}
         </h3>
 
         <div className='flex justify-center gap-4 text-gray-500 mb-4'>
