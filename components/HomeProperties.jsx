88585c464f50f359af9fceeea356d93878150a88
Thomas Martinez <devtommie114@gmail.com>
2024-08-08 22:11:12 -0400

    HomeProperites component / showing only 3 properites

---
+++ components/HomeProperties.jsx
@@ -0,0 +1,26 @@
+import properties from '@/properties.json';
+import PropertyCard from './PropertyCard';
+
+const HomeProperties = () => {
+  const recentProperties = properties.slice(0, 3);
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+          Recent Properties
+        </h2>
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {recentProperties.map(property => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default HomeProperties;
