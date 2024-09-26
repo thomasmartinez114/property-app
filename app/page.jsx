15d4be53d75f1f097bcf4fc0659991d3b0a65b79
Thomas Martinez <devtommie114@gmail.com>
2024-09-26 01:33:41 -0400

    configured Mongo and created Models

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
+import connectDB from '@/config/database';
 
 const HomePage = () => {
   return (
---
+++ config/database.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.startSession('strictQuery', true);
+
+  // if db is already connected, don't connect again
+  if (connected) {
+    console.log('MongoDB is connected');
+    return;
+  }
+
+  // Connect to MongoDB
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
