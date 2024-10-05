3d76c3cc7fbc1abf2f9881d49367d7c2a353dd19
Thomas Martinez <devtommie114@gmail.com>
2024-10-04 23:19:36 -0400

    image of property rendering

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,17 @@
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
-import { connect } from 'mongoose';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
   const property = await Property.findById(params.id).lean();
 
-  return <section>{property.name}</section>;
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>{property.name}</section>
+    </>
+  );
 };
 
 export default PropertyPage;
