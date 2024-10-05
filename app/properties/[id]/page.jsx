bb870cb93e43ba522733dc0c9f4ac41c2d816a1c
Thomas Martinez <devtommie114@gmail.com>
2024-10-04 23:14:06 -0400

    rendering property name

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,12 @@
-const PropertyPage = ({ params }) => {
-  return <div>Property Page {params.id}</div>;
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import { connect } from 'mongoose';
+
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const property = await Property.findById(params.id).lean();
+
+  return <section>{property.name}</section>;
 };
 
 export default PropertyPage;
