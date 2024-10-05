0479ebfb43d8d248a815b26a7f3c58f74effebd4
Thomas Martinez <devtommie114@gmail.com>
2024-10-04 23:22:06 -0400

    go back

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,8 @@
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -9,6 +11,16 @@ const PropertyPage = async ({ params }) => {
   return (
     <>
       <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
       <section>{property.name}</section>
     </>
   );
