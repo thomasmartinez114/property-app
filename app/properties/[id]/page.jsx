6e0f9aa75dac0f64239edd90f5fac57c26e5678a
Thomas Martinez <devtommie114@gmail.com>
2024-10-06 23:26:47 -0400

    updated return button

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -17,7 +17,7 @@ const PropertyPage = async ({ params }) => {
             href='/properties'
             className='text-blue-500 hover:text-blue-600 flex items-center'
           >
-            <FaArrowLeft className='mr-2' /> Back to Properties
+            <FaArrowLeft className='mr-2' /> Return to Properties
           </Link>
         </div>
       </section>
