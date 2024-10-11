38b72536f9815017dafcb85802b216a5d72447dd
Thomas Martinez <devtommie114@gmail.com>
2024-10-10 23:35:04 -0400

    propertyDetails hard coded will change to update from db

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,5 @@
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import PropertyDetails from '@/components/PropertyDetails';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import Link from 'next/link';
@@ -23,7 +24,9 @@ const PropertyPage = async ({ params }) => {
       </section>
       <section class='bg-blue-50'>
         <div class='container m-auto py-10 px-6'>
-          <div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'></div>
+          <div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            <PropertyDetails property={property} />
+          </div>
         </div>
       </section>
     </>
