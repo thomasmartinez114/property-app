18a22ddf1dc8346f2297bd56ee48a35065fd091c
Thomas Martinez <devtommie114@gmail.com>
2024-10-12 23:45:17 -0400

    added comma in zip

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -9,7 +9,7 @@ const PropertyDetails = ({ property }) => {
         <div className='text-gray-500 mb-4 flex align-middle justify-center md:justify-start'>
           <i className='fa-solid fa-location-dot text-lg text-orange-700 mr-2'></i>
           <p className='text-orange-700'>
-            {property.location.street}, {property.location.city}{' '}
+            {property.location.street}, {property.location.city},{' '}
             {property.location.zipcode}
           </p>
         </div>
