225dda63d24b8dfa3ad5b641a0d198996cf27e96
Thomas Martinez <devtommie114@gmail.com>
2024-10-14 23:16:08 -0400

    update

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -86,7 +86,7 @@ const PropertyDetails = ({ property }) => {
       </div>
       {/* <!-- Map --> */}
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
-        <div id='map'></div>
+        <div id='map'>MAP HERE</div>
       </div>
     </main>
   );
