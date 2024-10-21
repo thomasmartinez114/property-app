ebd68b0809dff3f851e2368e1f15cd4ad7e7ff41
Thomas Martinez <devtommie114@gmail.com>
2024-10-21 19:29:42 -0400

    commit

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -15,7 +15,7 @@ const HomeProperties = async () => {
     <>
       <section className='px-4 py-6'>
         <div className='container-xl lg:container m-auto px-4 py-6'>
-          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+          <h2 className='text-3xl font-bold text-blue-600 mb-6 text-center'>
             Recent Properties
           </h2>
           {recentProperties.length === 0 ? (
