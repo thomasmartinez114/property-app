85729efdff4b5eb2cad5fd3a34711452761b8a0d
Thomas Martinez <devtommie114@gmail.com>
2024-11-19 22:46:40 -0500

    GET properties;

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,16 @@
-export const GET = () => {
-  return new Response(JSON.stringify({ message: 'Hello World' }), {
-    status: 200,
-  });
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const properties = await Property.find({});
+
+    return new Response(properties, {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response('Something went wrong', { status: 500 });
+  }
 };
