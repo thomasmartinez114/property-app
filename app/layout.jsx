ee99fe5bd9c6b86a7f6b918ddcad5c0d6f61e993
Thomas Martinez <devtommie114@gmail.com>
2024-11-28 15:44:57 -0500

    next-auth and google provider setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
+import AuthProvider from '@/components/AuthProvider';
 import Footer from '@/components/Footer';
 
 export const metadata = {
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
